69f351390df179a3c65b1e9343a998608ba8dd24
AgathePlunian <agathe.plunian@gmail.com>
2020-01-28 17:34:10 +0100

    update

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'pry'
+require_relative 'lib/game'
+require_relative 'lib/player'
+
+player1 = HumanPlayer.new("petite_merde")
+player2 = HumanPlayer.new("Coco")
+
+
+puts "\n---- START FIGHT ! ----"
+print "\n"    
+
+while player1.life_points >= 0 && player2.life_points >= 0 do
+    print " \n"
+    puts "Voici l'état des joueurs:"
+    player2.search_weapon
+    player1.search_health_pack
+    
+    player1.show_state
+    player2.show_state
+    player1.attacks(player2)
+    if player2.life_points >= 0 
+        player2.attacks(player1)
+    end
+    
+end
---
+++ lib/player.rb
@@ -0,0 +1,81 @@
+require 'pry'
+
+class Player
+  attr_accessor :name, :life_points
+
+  def initialize(name)
+    @name = name
+    @life_points = 10
+  end
+
+  def show_state 
+    if self.class == Player
+     	puts "#{@name} a #{@life_points} points de vie"
+    else 
+    	puts "#{@name} a #{@life_points} points de vie et a une arme de niveau #{@weapon_level}"
+    end
+  end
+
+  def gets_damage(x)
+    @life_points = @life_points - x
+    if @life_points <= 0
+	    puts "Le joueur #{@name} a été tué !"
+    end
+  end
+
+  def attacks(ennemy)
+    puts "#{@name} attaque #{ennemy.name} !"
+    num_damage = compute_damage
+    puts "Il lui inflige #{num_damage} points de dommages, aie !"
+    ennemy.gets_damage(num_damage)
+  end
+
+  def compute_damage
+	  return rand(1..6)
+    end
+  end
+
+class HumanPlayer < Player
+attr_accessor :weapon_level
+
+	def initialize (name)
+			@name = name
+			@life_points = 100
+			@weapon_level = 1
+	end
+
+	def compute_damage
+			rand(1..6) * @weapon_level
+	end
+
+	def search_weapon
+			found_level_weapon = rand(1..6)
+			puts "Tu as trouvé une arme de niveau #{found_level_weapon}"
+			if @weapon_level < found_level_weapon 
+					@weapon_level = found_level_weapon
+					puts "YOUHOU ! Elle est meilleure que ton arme actuelle: tu la prends !"
+			else 
+					puts "CHIENNE DE VIE ! Elle n'est pas mieux que ton arme actuelle la putain de sa mère !"
+			end
+	end
+
+	def search_health_pack
+		found_health = rand(1..6)
+		if found_health == 1
+			puts "Tu n'as rien trouvé..."
+		elsif found_health == [2..5]
+			@life_points += 50
+			if @life_points > 100
+				@life_points = 100
+			end
+		puts "Bravo, tu as trouvé un pack de +50 points de vie !"
+		elsif found_health == 6
+				@life_points += 80
+			if @life_points > 100
+				@life_points = 100
+		end
+		puts "Waouuuuuuuw, tu as trouvé un pack de +80 points de vie !"
+		end
+   end
+end
+
